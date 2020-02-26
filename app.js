635ea003b4759821bbf7464bd5a20bde704cd945
Toshik Langade <toshiklangade1998@gmail.com>
2020-02-27 00:12:47 +0530

    secondinnit

--- app.js
+++ app.js
@@ -136,7 +136,8 @@ app.post("/inputkey", function(req, res) {
     }
   });
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log("Server has started succesfully");
 });
